358610996a4451d39c271ae5330e5aa354c72ac1
Nelson <nelsonchenglin@gmail.com>
2024-01-03 15:42:47 -0500

    Material UI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { CssBaseline } from "@mui/material"
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
-
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   )
 }
